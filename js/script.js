3405b1d77c7fbf35249858d274dfd6f1326126c6
Maddy <apotatoiswild@gmail.com>
2024-10-22 15:41:04 +1300

    Modal JS and Image swapping JS completed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@
 const btndisplayXY = document.getElementById('displayXY');
 const btnthemeSwap = document.getElementById('themeSwap');
 const btnmodalShow = document.getElementById('modalShow');
-const btnimageSwap = document.getElementById('imageSwap');
+const btnimageSwap = document.getElementById('swapImage');
+
+// Formatting of HTML elements to fix page
+document.getElementById('modal').style.display = 'none'; // Hide modal, doesn't load on first click unless this is here
+document.getElementById('mouseXp').style.display = "none"; // Hide Mouse X, table box is larger than intended if this isn't here
+document.getElementById('mouseYp').style.display = "none"; // Hide Mouse Y, table box is larger than intended if this isn't here
 
 // Create listen events
 btndisplayXY.addEventListener('mouseover', function() {
@@ -26,7 +31,7 @@ btnmodalShow.addEventListener('click', function() {
 });
 
 btnimageSwap.addEventListener('click', function() {
-    imageSwap();
+    swapImage();
 });
 
 // GENERAL FUNCTIONS
@@ -71,3 +76,40 @@ function themeSwap() {
 }
 
 // modalShow
+function modalShow() {
+    let modal = document.getElementById('modal');
+    // console.log(modal.style.display);
+    // If the modal is hidden, show modal
+    if (modal.style.display == 'none') {
+        modal.style.display = 'block';
+        // console.log('if branch');
+    }
+    // If the modal is shown, hide it
+    else {
+        modal.style.display = 'none';
+        // console.log('else branch');
+    }
+}
+
+// swapImage
+function swapImage() {
+    // Array of the live server urls. This is the only way I can get it to work.
+    let images = ['http://127.0.0.1:5500/img/business.jpg', 'http://127.0.0.1:5500/img/people.jpg', 'http://127.0.0.1:5500/img/sports.jpg'];
+    let image = document.getElementById('image'); // Get the image element
+    let modalImage = document.getElementById('modal-image'); // Get the modal-image element
+    if (image.src == images[0]) {
+        // console.log('swap to people');
+        image.src = 'img/people.jpg';
+        modalImage.src = 'img/people.jpg';
+    }
+    else if (image.src == images[1]) {
+        // console.log('swap to sports');
+        image.src = 'img/sports.jpg';
+        modalImage.src = 'img/sports.jpg';
+    }
+    else {
+        // console.log('swap to business');
+        image.src = 'img/business.jpg';
+        modalImage.src = 'img/business.jpg';
+    }
+}
